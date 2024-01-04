c0fa4d4d833efbdc7c71a9985935fd29b0356b18
Haruhi <haruhi1056`chiba-u.jp>
2024-01-04 18:39:39 +0900

    20240104 add sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<utility>
+#include<set>
+#include<unordered_set>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+using Graph = vector<vector<int>>;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+int main(){
+
+
+    return 0;
+}
